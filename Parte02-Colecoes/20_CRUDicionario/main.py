840adc06280fab0e8b9a3f015ae4d023fbd33651
tblima26 <tblima26@gmail.com>
2026-08-07 21:36:01 -0300

    CRUDicionario Finalizado - Data: 07/08/2026

--- Parte02-Colecoes/20_CRUDicionario/main.py
+++ Parte02-Colecoes/20_CRUDicionario/main.py
@@ -16,6 +16,7 @@ def menu():
   print("=" * 30)
 
 def cadastrar():
+    print("\n--- Cadastrar Usuários ---")
     usuario = {}
     usuario["nome"] = input('Nome: ').capitalize().strip()
     usuario["cpf"] = input('CPF: ').capitalize().strip()
@@ -28,14 +29,46 @@ def listar():
         for chave, valor in usuario.items():
             print(f"{chave.capitalize()}: {valor}")
         print(f"{'-' *40}")
+    input('Aperte algo para continuar...')
 
 def alterar():
-   #TODO - Fazer depois
-   return
+    print("\n--- Alterar Usuários ---")
+    nome = input('Pesquisar o Nome: ').capitalize().strip()
+    encontrado = False
+    for usuario in usuarios:
+        if nome in usuario['nome']:
+            encontrado = True
+            print(f'{usuario["nome"]}')
+            print(f'{usuario["cpf"]}')
+            print(f'{usuario["email"]}')
+            novo_nome = input(f"Novo nome: ").strip().title()
+            novo_cpf = input(f"Novo CPF: ").strip()
+            novo_email = input(f"Novo email: ").strip().lower()
+            if novo_nome:
+                usuario['nome'] = novo_nome
+            if novo_cpf:
+                usuario['cpf'] = novo_cpf
+            if novo_email:
+                usuario['email'] = novo_email
+            print("\nDados atualizados com sucesso!")
+    if not encontrado:
+        print(f'{nome} não encontrado.')
+        input('\n\nAperte algo para continuar...')
+
 
 def remover():
-   #TODO - Fazer depois
-   return
+    nome = input(f' -- Deletar -- \n\nNome: ').strip().title()
+    encontrado = False
+    for usuario in usuarios:
+      if nome in usuario['nome']:
+         encontrado = True
+         usuarios.remove(usuario)
+         print(f'{nome} deletado com sucesso!')
+         input('Aperte algo para continuar...')
+    if not encontrado:
+      print(f'{nome} não encontrado.')
+      input('\n\nAperte algo para continuar...')
+
 
 #SECTION - Main
 usuarios = []
@@ -46,18 +79,23 @@ while(True):
   limpaTela()
   match opcao:
     case '1':
+        limpaTela()
         usuarios.append(cadastrar())
         continue
     case '2':
+        limpaTela()
         listar()
         continue
     case '3':
+        limpaTela()
         alterar()
         continue
     case '4':
+        limpaTela()
         remover()
         continue
     case '5':
+        limpaTela()
         break
     case _:
         input('\nOpção Invalida.\nContinuar...')
