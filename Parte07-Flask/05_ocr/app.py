8204bb9487b1309b3d1ab5be9ecdd2954226c400
Thiago Borges <tblima26@gmail.com>
2026-09-18 21:36:09 -0300

    Commit do dia 18/09/26

--- Parte07-Flask/05_ocr/app.py
+++ Parte07-Flask/05_ocr/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/textoExtraido')
+def extrair_texto():
+  return render_template('extracao.html')
+
 if __name__ == "__main__":
   app.run(debug=True)
